88dc784af22050476f2ecc4113dfd7052a4156ce
mgruber161 <74315620+mgruber161@users.noreply.github.com>
2022-11-07 18:21:57 +0100

    Initial commit

---
+++ src/app/measurement-input/measurement-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-measurement-input',
+  templateUrl: './measurement-input.component.html',
+  styleUrls: ['./measurement-input.component.css']
+})
+export class MeasurementInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
