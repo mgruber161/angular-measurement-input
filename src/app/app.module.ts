6423940ac066c3c5072b941cc87a46e606843454
mgruber161 <74315620+mgruber161@users.noreply.github.com>
2022-11-07 18:29:32 +0100

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import {MatInputModule} from '@angular/material/input';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, MatInputModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/measurement-input/measurement-input.component.ts
+++ src/app/measurement-input/measurement-input.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-measurement-input',
   templateUrl: './measurement-input.component.html',
-  styleUrls: ['./measurement-input.component.css']
+  styleUrls: ['./measurement-input.component.css'],
 })
 export class MeasurementInputComponent implements OnInit {
+  value = 17.3;
+  sensor = 'Temperatur Wohnzimmer';
+  units = ['°C', 'Watt', 'ppm'];
+  unit = this.units[0];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
